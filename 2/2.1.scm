644271fc28981146e9b56fbe214fd4ad1e70eb10
rjhallsted <rjhallsted@gmail.com>
2019-12-14 13:37:08 -0700

    Refactor make-rat

--- 2/2.1.scm
+++ 2/2.1.scm
@@ -9,6 +9,14 @@
             (else
                 (cons (/ n g) (/ d g))))))
 
+(define (make-rat n d)
+    (let ((g (gcd (abs n) (abs d))))
+        (let ((num (abs (/ n g))) (denom (abs (/ d g))))
+            (cond ((or (and (< d 0) (> n 0)) (and (< n 0) (> d 0)))
+                    (cons (- num) denom))
+                (else
+                    (cons num denom))))))
+
 (define neg-one-half (make-rat 1 (- 2)))
 (print-rat neg-one-half)
 

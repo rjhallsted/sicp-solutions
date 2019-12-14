98c3093b91b6be5fef4c82b4af98ad5c6fec4f09
rjhallsted <rjhallsted@gmail.com>
2019-12-14 13:38:14 -0700

    Rename vars for clarity

--- 2/2.1.scm
+++ 2/2.1.scm
@@ -11,11 +11,11 @@
 
 (define (make-rat n d)
     (let ((g (gcd (abs n) (abs d))))
-        (let ((num (abs (/ n g))) (denom (abs (/ d g))))
+        (let ((simp-n (abs (/ n g))) (simp-d (abs (/ d g))))
             (cond ((or (and (< d 0) (> n 0)) (and (< n 0) (> d 0)))
-                    (cons (- num) denom))
+                    (cons (- simp-n) simp-d))
                 (else
-                    (cons num denom))))))
+                    (cons simp-n simp-d))))))
 
 (define neg-one-half (make-rat 1 (- 2)))
 (print-rat neg-one-half)

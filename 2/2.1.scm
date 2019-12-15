040afecf94657a28df10e0001f0158cc9cc9bafb
rjhallsted <rjhallsted@gmail.com>
2019-12-14 17:40:36 -0700

    Remove old version of make-rat

--- 2/2.1.scm
+++ 2/2.1.scm
@@ -2,15 +2,6 @@
 
 (define (make-rat n d)
     (let ((g (gcd (abs n) (abs d))))
-        (cond ((and (< n 0) (< d 0))
-                (cons (abs (/ n g)) (abs (/ d g))))
-            ((< d 0)
-                (cons (- (abs (/ n g))) (abs (/ d g))))
-            (else
-                (cons (/ n g) (/ d g))))))
-
-(define (make-rat n d)
-    (let ((g (gcd (abs n) (abs d))))
         (let ((simp-n (abs (/ n g))) (simp-d (abs (/ d g))))
             (cond ((or (and (< d 0) (> n 0)) (and (< n 0) (> d 0)))
                     (cons (- simp-n) simp-d))

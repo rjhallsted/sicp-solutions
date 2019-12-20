c3987651228f08abce29d75b10d0c250f172e4cb
rjhallsted <rjhallsted@gmail.com>
2019-12-20 14:34:24 -0700

    switch to using square

--- 2/2.21.scm
+++ 2/2.21.scm
@@ -3,13 +3,12 @@
 (define (square-list items)
   (if (null? items)
       nil
-      (cons (* (car items) (car items))
+      (cons (square (car items))
             (square-list (cdr items)))))
 
 ;(display (square-list (list 1 2 3 4))) (newline)
 
 (define (square-list items)
-  (map (lambda (x) (* x x))
-        items))
+  (map square items))
 
 ;(display (square-list (list 1 2 3 4))) (newline)

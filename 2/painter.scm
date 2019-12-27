ed2851f7c19f05c2d234f59e3876f6174f69260a
rjhallsted <rjhallsted@gmail.com>
2019-12-27 11:22:34 -0700

    ex 2.45

---
+++ 2/painter.scm
@@ -0,0 +1,40 @@
+(define (corner-split painter n)
+    (if (= n 0)
+        painter
+        (let ((up (up-split painter (- n 1)))
+                (right (right-split painter (- n 1))))
+            (let ((top-left (beside up up))
+                (bottom-right (below right right))
+                (corner (corner-split painter (- n 1))))
+            (beside (below painter top-left)
+                    (below bottom-right corner))))))
+
+(define (right-split painter n)
+    (if (= n 0)
+        painter
+        (let ((smaller (right-split painter (- n 1))))
+            (beside painter (below smaller smaller)))))
+
+
+(define (up-split painter n)
+    (if (= n 0)
+        painter
+        (let ((smaller (up-split painter (- n 1))))
+            (below painter (beside smaller smaller)))))
+
+(define (square-of-four tl tr bl br)
+    (lambda (painter)
+        (let ((top (beside (tl painter) (tr painter)))
+                (bottom (beside (bl painter) (br painter))))
+            (below bottom top))))
+
+(define (flipped-pairs painter)
+    (let ((combine4 (square-of-four identity flip-vert
+                                    identity flip-vert)))
+        (combine4 painter)))
+
+(define (square-limit painter n)
+    (let ((combine4 (square-of-four flip-horiz identity
+                                    rotate180 flip-vert)))
+        (combine4 (corner-split painter n))))
+

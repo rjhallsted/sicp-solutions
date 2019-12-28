0fcee2166307fee8892557b7d10d8b5b939fcb4f
rjhallsted <rjhallsted@gmail.com>
2019-12-28 11:34:31 -0700

    Ex 2.54

---
+++ 2/2.3/2.54.scm
@@ -0,0 +1,8 @@
+(define (equal? a b)
+    (if (and (pair? a) (pair? b))
+        (and (equal? (car a) (car b))
+            (equal? (cdr a) (cdr b)))
+        (eq? a b)))
+
+(display (equal? '(this is a list) '(this is a list))) (newline)
+(display (equal? '(this is a list) '(this (is a) list)))) (newline)

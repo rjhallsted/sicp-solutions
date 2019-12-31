351ad3247e8c5264e444be1d63c30d5ef9d37814
rjhallsted <rjhallsted@gmail.com>
2019-12-30 20:28:28 -0700

    ex 2.57

--- 2/2.3/2.57.scm
+++ 2/2.3/2.57.scm
@@ -1,12 +1,4 @@
-(load "../2.2/list-ops.scm")
-
-(define (=number? exp num)
-    (and (number? exp) (= exp num)))
-
-(define (variable? x) (symbol? x))
-
-(define (same-variable? v1 v2)
-    (and (variable? v1) (variable? v2) (eq? v1 v2)))
+(load "differentiation.scm")
 
 (define (deriv-reduce proc number-proc init exp)
     (if (pair? exp)
@@ -40,57 +32,18 @@
             (else (list '* m1 m2))))
     (deriv-reduce '* * 1 (make-product-inner a b)))
 
-(define (sum? x)
-    (and (pair? x) (eq? (car x) '+)))
-
-(define (addend s) (cadr s))
-
-(define (augend s) (caddr s))
 (define (augend s)
     ;; (display s) (newline)
     (if (pair? (cdddr s))
         (make-sum (caddr s) (augend (append (list '+) (cddr s))))
         (caddr s)))
 
-(define (product? x)
-    (and (pair? x) (eq? (car x) '*)))
-
-(define (multiplier p) (cadr p))
-
-(define (multiplicand p) (caddr p))
 (define (multiplicand p)
     (if (pair? (cdddr p))
         (make-product (caddr p) (multiplicand (append (list '*) (cddr p))))
         (caddr p)))
 
 
-(define (exponentiation? x)
-    (and (pair? x) (eq? (car x) '**)))
-
-(define (base ex) (cadr ex))
-
-(define (exponent ex) (caddr ex))
-
-(define (make-exponentiation base power)
-    (list '** base power))
-
-(define (deriv exp var)
-    (cond ((number? exp) 0)
-        ((variable? exp)
-            (if (same-variable? exp var) 1 0))
-        ((sum? exp)
-            (make-sum (deriv (addend exp) var)
-                    (deriv (augend exp) var)))
-        ((product? exp)
-            (make-sum
-                (make-product (multiplier exp)
-                                (deriv (multiplicand exp) var))
-                (make-product (deriv (multiplier exp) var)
-                                (multiplicand exp))))
-        ((exponentiation? exp)
-            (make-product (make-product (exponent exp)
-                                        (make-exponentiation (base exp)
-                                                        (- (exponent exp) 1)))
-                        (deriv (base exp) var)))
-        (else
-            (error "unknown expression type -- DERIV" exp))))
+(display (deriv '(* (* 2 x) (* 3 x)) 'x)) (newline)
+(display (deriv '(+ (* 3 x) x) 'x)) (newline)
+(display (deriv '(+ (+ (* 3 x) x) x) 'x)) (newline)

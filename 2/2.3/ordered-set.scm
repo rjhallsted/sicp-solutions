32b3767c48c2b845eb12d90cc2b694e4cf70b236
rjhallsted <rjhallsted@gmail.com>
2020-01-07 13:41:37 -0700

    ex 2.62.scm

--- 2/2.3/ordered-set.scm
+++ 2/2.3/ordered-set.scm
@@ -22,3 +22,15 @@
         ((= x (car set)) set)
         ((> x (car set)) (cons (car set) (adjoin-set x (cdr set))))
         (else (cons x set))))
+
+(define (union-set set1 set2)
+    (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else
+            (let ((x1 (car set1)) (x2 (car set2)))
+                (cond ((= x1 x2)
+                        (cons x1 (union-set (cdr set1) (cdr set2))))
+                    ((< x1 x2)
+                        (cons x1 (union-set (cdr set1) set2)))
+                    ((< x2 x1)
+                        (cons x2 (union-set set1 (cdr set2)))))))))

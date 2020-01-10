e84c3526242ea6abeef387ce02322ff95acb8ac2
rjhallsted <rjhallsted@gmail.com>
2020-01-10 12:30:28 -0700

    Ex 2.71

---
+++ 2/2.3/2.71.scm
@@ -0,0 +1,34 @@
+;n=5
+;;   *
+;;  / \
+;; a   *
+;;    / \
+;;   b   *
+;;      / \
+;;     c   *
+;;        / \
+;;       d   e
+
+;n=10
+;;   *
+;;  / \
+;; a   *
+;;    / \
+;;   b   *
+;;      / \
+;;     c   *
+;;        / \
+;;       d   *
+;;          / \
+;;         e   *
+;;            / \
+;;           f   *
+;;              / \
+;;             g   *
+;;                / \
+;;               h   *
+;;                  / \
+;;                 i   j
+
+
+;In general, the most frequent symbol will require 1 bit. The least frequent symbol will require n-1 bits.

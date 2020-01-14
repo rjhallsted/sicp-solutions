3058f77c3cff72b57e5596c371958d8c49a92b92
rjhallsted <rjhallsted@gmail.com>
2020-01-14 10:54:38 -0600

    Ex 2.73

--- 2/2.4/2.73.scm
+++ 2/2.4/2.73.scm
@@ -25,12 +25,14 @@
         (and (number? exp) (= exp num)))
     ;; interface to the rest of the system
     (put 'deriv '+ (lambda (exp var) (sum-deriv exp var)))
+    (put 'make '+ (lambda (a1 a2) (make-sum a1 a2)))
     'done)
 
+;Note that this is dependent on the deriv-sum package
 (define (install-deriv-product-package)
     ;; internal procedures
     (define (product-deriv exp var)
-        (make-sum
+        ((get 'make '+)
             (make-product (multiplier exp)
                         (deriv (multiplicand exp) var))
             (make-product (deriv (multiplier exp) var)
@@ -41,15 +43,34 @@
             ((=number? m2 1) m1)
             ((and (number? m1) (number? m2)) (* m1 m2))
             (else (list '* m1 m2))))
-    (define (make-sum a1 a2)
-        (cond ((=number? a1 0) a2)
-            ((=number? a2 0) a1)
-            ((and (number? a1) (number? a2)) (+ a1 a2))
-            (else (list '+ a1 a2))))
     (define (multilplier s) (car s))
     (define (multiplicand s) (caddr s))
     (define (=number? exp num)
         (and (number? exp) (= exp num)))
     ;; interface to the rest of the system
     (put 'deriv '* (lambda (exp var) (product-deriv exp var)))
+    (put 'make '* (lambda (m1 m2) (make-product m1 m2)))
     'done)
+
+;c)
+
+;Note that this is dependent on the deriv-product package
+(define (install-deriv-exponent-package)
+    ;; internal procedures
+    (define (exponent-deriv exp var)
+        (let ((make-product (get 'make '*)))
+            (make-product (make-product (exponent exp)
+                                        (make-exponentiation (base exp)
+                                                            (- (exponent exp) 1)))
+                            (deriv (base exp) var))))
+    (define (base ex) (cadr ex))
+    (define (exponent ex) (caddr ex))
+    (define (make-exponentiation base power)
+        (list '** base power))
+    ;; interface to the rest of the system
+    (put 'deriv '** (lambda (exp var) (exponent-deriv exp var)))
+    (put 'make '** (lambda (m1 m2) (make-exponentiation m1 m2)))
+    'done)
+
+;d) We would only need to adjust the uses of get, and swap the order of the first two arguments
+; in the uses of put. 

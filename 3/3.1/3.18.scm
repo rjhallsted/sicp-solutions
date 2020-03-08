4246e2e7f72d37321ecc4c77616e442f4cf55a85
rjhallsted <rjhallsted@gmail.com>
2020-03-08 14:05:12 +1300

    Ex 3.18

---
+++ 3/3.1/3.18.scm
@@ -0,0 +1,19 @@
+(define (is-cycled? x)
+    (define (is-cycled-inner first-pair x)
+        (cond ((null? x) #f)
+            ((eq? x first-pair) #t)
+            (else (is-cycled-inner first-pair (cdr x)))))
+    (is-cycled-inner x (cdr x)))
+
+(define (last-pair x)
+    (if (null? (cdr x))
+        x
+        (last-pair (cdr x))))
+(define (make-cycle x)
+    (set-cdr! (last-pair x) x)
+    x)
+
+(define d '(a b c))
+(display (is-cycled? d)) (newline)
+(make-cycle d)
+(display (is-cycled? d)) (newline)

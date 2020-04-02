de39c0a91ec4e701ca214671908449f1d6320f47
rjhallsted <rjhallsted@gmail.com>
2020-04-03 00:51:10 +1300

    Fix 3.77

--- 3/3.5/3.77.scm
+++ 3/3.5/3.77.scm
@@ -2,9 +2,9 @@
 (define (integral delayed-integrand initial-value dt)
     (cons-stream
         initial-value
-        (if (stream-null? integrand)
-            the-empty-stream
-            (let ((integrand (force delayed-integrand)))
-                (integral (stream-cdr integrand)
+        (let ((integrand (force delayed-integrand)))
+            (if (stream-null? integrand)
+                the-empty-stream
+                (integral (delay (stream-cdr integrand))
                         (+ (* dt (stream-car integrand)) initial-value)
                         dt)))))
